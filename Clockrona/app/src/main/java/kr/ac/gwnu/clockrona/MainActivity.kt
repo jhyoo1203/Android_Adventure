1378e7a236f6184d617229e12e6ecfc837c575f4
YooJaeHyeon <68884608+YooJaeHyeon@users.noreply.github.com>
2020-11-12 18:48:04 +0900

    Clockrona 앱 개발

---
+++ Clockrona/app/src/main/java/kr/ac/gwnu/clockrona/MainActivity.kt
@@ -0,0 +1,20 @@
+package kr.ac.gwnu.clockrona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.menu)
+
+        toolbar.setOnClickListener {
+
+        }
+    }
+}
