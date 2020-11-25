2919ae8b63c9c7cec7533a17b916bcb955f00111
YooJaeHyeon <68884608+YooJaeHyeon@users.noreply.github.com>
2020-11-25 11:26:28 +0900

    Update DBHelper.kt

--- CVD19INFO/app/src/main/java/kr/ac/gwnu/mobile/cvd19info/DBHelper.kt
+++ CVD19INFO/app/src/main/java/kr/ac/gwnu/mobile/cvd19info/DBHelper.kt
@@ -11,20 +11,42 @@ class DBHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         private val DATABASE_NAME = "CLOCKRONA"
 
         //Table
-        private val TABLE_NAME = "AfterEffectTBL"
+        private val TABLE_NAME1 = "SymptomTBL"
+        private val SYM_ID = "Id"
+        private val SYM_NAME = "Name"
+        private val SYM_INCIDENCE = "Incidence"
+
+        private val TABLE_NAME2 = "AfterEffectTBL"
         private val EFF_ID = "Id"
         private val EFF_NAME = "Name"
         private val EFF_INCIDENCE = "Incidence"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val CREATE_TABLE_QUERY = ("CREATE TABLE $TABLE_NAME ($EFF_ID INTEGER PRIMARYKEY, $EFF_NAME TEXT, $EFF_INCIDENCE INTEGER)")
-        db!!.execSQL(CREATE_TABLE_QUERY)
+        val CREATE_TABLE_QUERY1 = ("CREATE TABLE $TABLE_NAME1 ($SYM_ID INTEGER PRIMARYKEY, $SYM_NAME TEXT, $SYM_INCIDENCE INTEGER)")
+        db!!.execSQL(CREATE_TABLE_QUERY1)
+
+        val CREATE_TABLE_QUERY2 = ("CREATE TABLE $TABLE_NAME2 ($EFF_ID INTEGER PRIMARYKEY, $EFF_NAME TEXT, $EFF_INCIDENCE INTEGER)")
+        db!!.execSQL(CREATE_TABLE_QUERY2)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_NAME1")
         onCreate(db!!)
+
+        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_NAME2")
+        onCreate(db!!)
+    }
+
+    fun addSymptom(id: Int, name: String, incidence: Int) {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(SYM_ID, id)
+        values.put(SYM_NAME, name)
+        values.put(SYM_INCIDENCE, incidence)
+
+        db.insert(TABLE_NAME1, null, values)
+        db.close()
     }
 
     fun addAfterEffect(id: Int, name: String, incidence: Int) {
@@ -34,7 +56,7 @@ class DBHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         values.put(EFF_NAME, name)
         values.put(EFF_INCIDENCE, incidence)
 
-        db.insert(TABLE_NAME, null, values)
+        db.insert(TABLE_NAME2, null, values)
         db.close()
     }
 }
